d2140ffedfaf132b128e3e3ac3ee6d66c6e3d77b
Guilherme Lima <guilhermeguiguigugu2509@gmail.com>
2025-12-09 19:25:42 +0000

    Aplicando estilos basicos de peso das fontes

--- themovie.app/src/components/SectionTittle/SectionTittle.jsx
+++ themovie.app/src/components/SectionTittle/SectionTittle.jsx
@@ -1,8 +1,10 @@
+import "./SectionTittle.css"
+
 const SectionTittle = ({ title, subTittle }) => {
     return (
         <>
-            <h1>{title}</h1>
-            {subTittle && <p>{subTittle}</p>}
+            <h1 className="tittle-text">{title}</h1>
+            {subTittle && <p className="subtittle-text">{subTittle}</p>}
         </>
     )
 }
