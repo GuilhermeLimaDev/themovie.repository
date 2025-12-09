8fdd62eadae5e263f4f546f310f05e4151752135
GuilhermeLimaDev <guilhermeguiguigugu2509@gmail.com>
2025-12-09 12:15:03 -0300

    Adicionando componente Homepage com estrutura básica

---
+++ themovie.app/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import MainLayout from "../layout/MainLayout/MainLayout"
+
+const Homepage = () => {
+    return (
+        <>
+            <MainLayout>
+                <h1>Homepage</h1>
+            </MainLayout>
+        </>
+    )
+}
+
+export default Homepage
